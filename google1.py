bd220cdb1298ec74faac464ea5c3451cd13f97a6
Khanif3693 <khanha@oregonstate.edu>
2018-10-18 19:26:32 -0700

    Add files via upload

---
+++ google1.py
@@ -0,0 +1,23 @@
+def solution(S):
+    S = S.replace(":", "")
+    ls = []
+
+    gt = int(S)
+    if (S[0] == S[1] == S[2] == S[3]):
+        return S[0] + S[1] + ":" + S[2] + S[3]
+    for i in range(len(S)):
+        for j in range(len(S)):
+            for k in range(len(S)):
+                for l in range(len(S)):
+                    if (int(S[i] + S[j] + S[k] + S[l]) < gt):
+                        ls.append(S[i] + S[j] + S[k] + S[l])
+
+    S = max(ls)
+    return S[0] + S[1] + ":" + S[2] + S[3]
+
+
+print(solution("22:22"))
+print(solution("22:21"))
+print(solution("21:22"))
+print(solution("10:00"))
+
